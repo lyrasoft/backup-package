9b31639f2f149519a62ce1f96d1c4d21c205e801
Simon Asika <asika32764@gmail.com>
2020-08-14 22:32:23 +0800

    script install

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # LYRASOFT 全站備份 Script
 
+## Install
+
+```
+php -r "copy('https://raw.githubusercontent.com/lyrasoft/backup-script/master/install_script.php', 'backup_install_script.php');"
+php backup_install_script.php
+```
+
 ## 使用方式
 
 WIP
--- install_script.php
+++ install_script.php
@@ -7,4 +7,53 @@
  * @license    __LICENSE__
  */
 
+copy('https://raw.githubusercontent.com/lyrasoft/backup-script/master/backup.php', 'backup.php');
 
+$file = __DIR__ . '/backup.php';
+$content = file_get_contents($file);
+$content = str_replace('{{ secret }}', bin2hex(random_bytes(16)), $content);
+
+$config = [];
+
+$y = ['y', '1', 'yes'];
+
+if (in_array(strtolower(ask("Do you want to use DB? [Y/n]") ?: 'y'), $y, true)) {
+    $host = ask("Host[localhost]: ") ?: 'localhost';
+    $name = ask("DB Name: ");
+    $user = ask("User[root]: ") ?: 'root';
+
+    fwrite(STDOUT, "Password: ");
+    system('stty -echo');
+
+    $password = trim(fgets(STDIN));
+
+    system('stty echo');
+
+    $content = str_replace(
+        [
+            "'host' => 'localhost'",
+            "'user' => ''",
+            "'pass' => ''",
+            "'name' => ''",
+        ],
+        [
+            "'host' => '$host'",
+            "'user' => '$user'",
+            "'pass' => '$password'",
+            "'name' => '$name'",
+        ],
+        $content
+    );
+}
+
+$content = file_put_contents($file, $content);
+
+[$self] = get_included_files();
+unlink($self);
+
+fwrite(STDOUT, "\nSuccess install backup.php file.");
+
+function ask($question) {
+    fwrite(STDOUT, $question);
+    return trim(fgets(STDIN), "\n");
+}
